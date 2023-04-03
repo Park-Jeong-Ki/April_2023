f7d1ed53d3c8a783867bfd734655003c76753269
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-04-03 15:42:03 +0900

    주석 추가

--- ex05/palindrome.pl
+++ ex05/palindrome.pl
@@ -1,14 +1,15 @@
-#!/usr/bin/perl
+#!/usr/bin/perl  # This line specifies that the script should be run with Perl.
 
-print "Enter a string: ";
+print "Enter a string: ";  # Asks the user to enter a string.
 
-$input = <STDIN>;
-chomp($input);
-if ($input eq reverse $input)
+$input = <STDIN>;  # Reads a line of input from the user.
+chomp($input);  # Removes the 개행 at the end of the input.
+
+if ($input eq reverse $input)  # Checks if the input string is the same as its reverse.
 {
-    print("The string is a palindrome!\n");
+    print("The string is a palindrome!\n");  # If it is, prints a message saying so.
 }
 else
 {
-    print("The string is not a palindrome.\n");
+    print("The string is not a palindrome.\n");  # If it isn't, prints a message saying so.
 }
